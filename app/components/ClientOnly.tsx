639eeb9c27d8aed42308c0e3000849addaa3abda
DylanIz <116808456+DylanIz@users.noreply.github.com>
2023-04-25 21:11:42 +0100

    Modals/Register
    Worked on getting Modals setup and started with Register feature

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
+interface ClientOnlyProps{
+    children: React.ReactNode;
+}
+
+const ClientOnly : React.FC<ClientOnlyProps> = ({
+    children
+}) => {
+    const [hasMounted, SetHasMounted] = useState(false);
+
+    useEffect(() => {
+        SetHasMounted(true);
+    }, []);
+
+    if (!hasMounted) {
+        return null;
+    }
+
+    return ( 
+        <>
+            {children}
+        </>
+
+     );
+}
+ 
+export default ClientOnly;
