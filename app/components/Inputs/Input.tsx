0913a6dc57397f052703cf43a5d06fe2adb29135
DylanIz <116808456+DylanIz@users.noreply.github.com>
2023-05-15 16:29:00 +0100

    Update Input.tsx

--- app/components/Inputs/Input.tsx
+++ app/components/Inputs/Input.tsx
@@ -58,7 +58,7 @@ const Input: React.FC<InputProps> = ({
                     disabled:cursor-not-allowed
                     ${formatPrice ? 'pl-9' : 'pl-4'}
                     ${errors[id] ? 'border-rose-500' : 'border-neutral-300'}
-                    ${errors[id] ? 'focus:border-rose-500' : 'border-neutral-300'}
+                    ${errors[id] ? 'focus:border-rose-500' : 'focus:border-black'}
                 `}
             />
         </div>
