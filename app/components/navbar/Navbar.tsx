3b798c9a2488d606299a2882e8f66fffbea3705d
DylanDrumgoole <dylan.drumgoole@fenergo.com>
2023-06-09 15:36:29 +0100

    Logged in user menu with sign out feature

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -6,8 +6,15 @@ import Logo from "./Logo";
 import Search from "../navbar/Search";
 import UserMenu from "./UserMenu";
 import 'tailwindcss/tailwind.css';
+import { User } from "@prisma/client";
 
-const Navbar = () => {
+interface NavbarProps {
+    currentUser?: User | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({
+    currentUser
+}) => {
     return ( 
         <div className="fixed w-full bg-white z-10 shadow-sm">
             <div
@@ -29,7 +36,7 @@ const Navbar = () => {
                     >
                         <Logo />
                         <Search />
-                        <UserMenu />
+                        <UserMenu currentUser={currentUser} />
                     </div>
                 </Container>
             </div>
